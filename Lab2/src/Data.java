ebe2daf17ae9f20d569538582f4c9906aa8c21ca
SeilkhanA <40297551+SeilkhanA@users.noreply.github.com>
2018-09-17 14:03:05 +0600

    Update Data.java

--- Lab2/src/Data.java
+++ Lab2/src/Data.java
@@ -1,31 +1,55 @@
-import java.util.Scanner;
+import  java.util.Scanner;
 
 public class Data {
+    private  double sum = 0;
+    private  double max = 0;
+    public   int k = 0;
 
-    private int input;
-    private double average;
-    private double max;
 
-    public Data(){
-
+    public  Data(){
     }
-        public static void main(String[] args)
-        {
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Enter the scores:");
-            double input = 0;
-            double num = 0;
-            double sum = 0;
 
-            while (input != '')
-            {
-                input = scan.nextDouble();
-                if (input != 'Q') {
-                    sum += input;
-                    num++;
-                }
-            }
-            System.out.println("Average = " + (sum/num));
+
+
+    public  void addToData(int n){
+
+        if(max < n){
+            max = n;               //find Maximum
         }
+        sum = sum + n;           //find middle
+        k++;
+    }
+
+    public  String result(){
+        return  "Average = " + sum/k + "\n" + "Maximum= " + max;
+    }
+
+    public  static  void main(String[] args){
+
+        System.out.println("Enter number (Q to quit ):");
+
+        Scanner sc = new Scanner(System.in);
+
+        Data d = new Data();
+
+        int n;
+
+        String s = sc.next();
+
+        while (!s.equals("Q")){         //пока не вводим Q
+
+            n = Integer.parseInt(s);          //convert string into int
+
+            d.addToData(n);                  //and make opearations of finding max and average
+
+            System.out.println("Enter number (Q to quit ):");
+
+            s = sc.next();             //для след ввода числа         //без сканнера будет бесконечно писать "Enter number (Q to quit ):"
+        }
+        if(s.equals("Q")){                                // иначе, если ввожу Q
+
+            System.out.println(d.result());                 // вывести функцию, дающую ответ
+        }
+    }
 
 }
